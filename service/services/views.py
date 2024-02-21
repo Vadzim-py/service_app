2b39fb53e7ce6247ede42df202c1af07ac803006
Vadzim <workvadzim@gmail.com>
2024-02-22 00:10:01 +0300

    Lesson #4 Annotate и Aggregate in ORM
    * Get_price function was created in services serializers
    * Added annotations in services in views.py, in SubscriptionView controller in queryset
    * List method overridden in services in views.py, in SubscriptionView controller

--- service/services/views.py
+++ service/services/views.py
@@ -1,23 +1,27 @@
-from django.db.models import Prefetch
-from rest_framework import serializers
+from django.db.models import Prefetch, F, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
-from services.models import Subscription, Plan
+from services.models import Subscription
 from services.serializers import SubscriptionSerializer
-
-
-class PlanSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Plan
-        fields = '__all__'
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
         Prefetch('client',
-                 queryset=Client.objects.all().select_related('user').only('company_name',
-                                                                           'user__email'))
-    )
+     queryset=Client.objects.all().select_related('user').only('company_name',
+                                                               'user__email'))
+        ).annotate(price=F('service__full_price') -
+                         F('service__full_price') * F('plan__discount_percent') / 100.00)
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        response = super().list(request, *args, **kwargs)
+
+        response_data = {'result': response.data}
+        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        response.data = response_data
+
+        return response
